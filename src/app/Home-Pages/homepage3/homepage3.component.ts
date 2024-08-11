05a0fec6cd10b3436a0e086a6f0fbf7a32d52e8e
ahsan-usman07 <ahsan.usman@ethosverntures.uk>
2024-08-11 06:43:43 -0700

    hompage 3 fixes

--- src/app/Home-Pages/homepage3/homepage3.component.ts
+++ src/app/Home-Pages/homepage3/homepage3.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { TextAnimationComponent } from '../../components/text-animation/text-animation.component';
 import { TeamComponent } from '../../components/team/team.component';
 import { HorizontalScrollCardsComponent } from '../../components/why-Us-Folder/horizontal-scroll-cards/horizontal-scroll-cards.component';
+import { CommonModule } from '@angular/common';
 
 gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-homepage3',
   standalone: true,
-  imports: [TextAnimationComponent, TeamComponent, HorizontalScrollCardsComponent],
+  imports: [TextAnimationComponent, TeamComponent, HorizontalScrollCardsComponent, CommonModule],
   templateUrl: './homepage3.component.html',
   styleUrl: './homepage3.component.scss'
 })
-export class Homepage3Component implements OnInit{
+export class Homepage3Component implements OnInit {
+  isMobileView: boolean = false;
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
   ngOnInit(): void {
     this.setupAnimations();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobileView = window.innerWidth <= 768;
   }
 
   setupAnimations(): void {
